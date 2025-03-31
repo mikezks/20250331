20aca807bcf08ea495e6520290aafeb3c834ace5
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-03-31 18:10:00 +0200

    demo: lazy loading interceptors

--- projects/flight/src/app/booking/feature-flight/flight-booking/flight-booking.component.ts
+++ projects/flight/src/app/booking/feature-flight/flight-booking/flight-booking.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Flight, FlightService } from '../../api-boarding';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -13,4 +15,11 @@ import { RouterLink, RouterOutlet } from '@angular/router';
     </div>
   `
 })
-export class FlightBookingComponent {}
+export class FlightBookingComponent {
+  private http = inject(HttpClient);
+
+  constructor() {
+    this.http.get<Flight>('https://demo.angulararchitects.io/api/flight?id=3')
+      .subscribe(console.log);
+  }
+}
