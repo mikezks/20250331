3cb8ab7e74092226cfcfed1c7aff582a90561ebf
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-03-31 15:48:53 +0200

    refactor: standalone schematics

--- projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
+++ projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
@@ -1,9 +1,9 @@
+import { AsyncPipe, JsonPipe, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { Flight, FlightFilter } from '../../logic-flight';
-import { TicketsFacade } from './../../logic-flight/+state/facade';
 import { FlightCardComponent } from '../../ui-flight/flight-card/flight-card.component';
-import { UiFlightModule } from '../../ui-flight/ui-flight.module';
-import { AsyncPipe, JsonPipe, NgFor, NgIf } from '@angular/common';
+import { FlightFilterComponent } from '../../ui-flight/flight-filter/flight-filter.component';
+import { TicketsFacade } from './../../logic-flight/+state/facade';
 
 
 @Component({
@@ -11,7 +11,7 @@ import { AsyncPipe, JsonPipe, NgFor, NgIf } from '@angular/common';
   imports: [
     NgFor, NgIf, JsonPipe, AsyncPipe,
     FlightCardComponent,
-    UiFlightModule
+    FlightFilterComponent
   ],
   templateUrl: './flight-search.component.html',
 })
