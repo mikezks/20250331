8c709d54dd2239d27e5e0dc2ba52d1e3e9ceca7e
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-03-31 17:48:32 +0200

    fix: auth state true

--- projects/flight/src/app/app.providers.ts
+++ projects/flight/src/app/app.providers.ts
@@ -2,5 +2,5 @@ import { InjectionToken } from "@angular/core";
 
 export const AUTH_STATE = new InjectionToken<boolean>('AUTH_STATE', {
     providedIn: 'root',
-    factory: () => false
+    factory: () => true
 });
