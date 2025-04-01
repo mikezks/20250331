174b67660913d5d757942f6f149242a2fa184193
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-04-01 17:43:00 +0200

    demo: signal internals

--- projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
+++ projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
@@ -3,6 +3,7 @@ import { Component, computed, effect, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Flight, FlightFilter, injectTicketsFacade } from '../../logic-flight';
 import { FlightCardComponent, FlightFilterComponent } from '../../ui-flight';
+import { SIGNAL } from '@angular/core/primitives/signals';
 
 
 @Component({
@@ -33,7 +34,13 @@ export class FlightSearchComponent {
   protected flights$ = this.ticketsFacade.flights$;
 
   constructor() {
-    effect(() => console.log(this.route()));
+    let activeConsumer = effect(
+      () => console.log(
+        this.route()
+      )
+    );
+
+    console.log(this.route[SIGNAL]);
   }
   
   protected search(filter: FlightFilter): void {
