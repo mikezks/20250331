f657e98d0beb5f4cc5617bd18c4e4fe9e788123d
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-04-01 15:26:02 +0200

    feat: local navigation providers and components

--- projects/flight/src/app/booking/feature-flight/flight-booking/flight-booking.component.ts
+++ projects/flight/src/app/booking/feature-flight/flight-booking/flight-booking.component.ts
@@ -2,21 +2,43 @@ import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { Flight, FlightService } from '../../api-boarding';
 import { HttpClient } from '@angular/common/http';
+import { NavigationComponent } from '../../../shared/ui-common';
+import { provideNavigationService } from '../../../shared/logic-navigation';
+import { NavigationProviderDirective } from '../../../shared/ui-common/navigation/navigation-provider.directive';
 
 
 @Component({
   selector: 'app-flight-booking',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    NavigationComponent,
+    NavigationProviderDirective
   ],
   template: `
+    <app-navigation class="nav-standalone" />
+    <app-navigation [navConfig]="navState" class="nav-standalone" />
+
     <div>
       <router-outlet></router-outlet>
     </div>
-  `
+  `,
+  providers: [
+    provideNavigationService([
+      {
+        route: 'flight/search',
+        label: 'Flight Search',
+        icon: 'flight'
+      },
+    ])
+  ]
 })
 export class FlightBookingComponent {
   private http = inject(HttpClient);
+  protected navState = [{
+    route: 'flight/departures',
+    label: 'Departures',
+    icon: 'departures'
+  }];
 
   constructor() {
     this.http.get<Flight>('https://demo.angulararchitects.io/api/flight?id=3')
