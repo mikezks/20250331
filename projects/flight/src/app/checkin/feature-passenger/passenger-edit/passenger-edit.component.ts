d9b7589cf82355a0110283bf92d952da372eabc3
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-04-02 16:10:28 +0200

    refactor: resource, rx resource

--- projects/flight/src/app/checkin/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ projects/flight/src/app/checkin/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,43 +1,47 @@
 import { NgIf } from '@angular/common';
-import { Component, effect, inject, input, numberAttribute, signal } from '@angular/core';
+import {
+  Component,
+  effect,
+  inject,
+  input,
+  numberAttribute,
+  signal,
+} from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { validatePassengerStatus } from '../../util-validation';
 import { initialPassenger } from '../../logic-passenger';
 import { PassengerService } from '../../logic-passenger/data-access/passenger.service';
 import { switchMap } from 'rxjs';
-
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-passenger-edit',
-  imports: [
-    NgIf,
-    ReactiveFormsModule
-  ],
-  templateUrl: './passenger-edit.component.html'
+  imports: [NgIf, ReactiveFormsModule, RouterLink],
+  templateUrl: './passenger-edit.component.html',
 })
 export class PassengerEditComponent {
   private passengerService = inject(PassengerService);
 
   id = input(0, { transform: numberAttribute });
-  passenger = toSignal(
-    toObservable(this.id).pipe(
-      switchMap(id => this.passengerService.findById(id))
-    ), { initialValue: initialPassenger }
-  );
+  passengerResource = this.passengerService.findByIdAsResource(this.id);
 
   protected editForm = inject(NonNullableFormBuilder).group({
     id: [0],
     firstName: [''],
     name: [''],
     bonusMiles: [0],
-    passengerStatus: ['', [
-      validatePassengerStatus(['A', 'B', 'C'])
-    ]]
+    passengerStatus: ['', [validatePassengerStatus(['A', 'B', 'C'])]],
   });
 
   constructor() {
-    effect(() => this.editForm.patchValue(this.passenger()));
+    effect(() => {
+      const passenger = this.passengerResource.value();
+
+      if (passenger) {
+        this.editForm.patchValue(passenger);
+      }
+    });
   }
 
   protected save(): void {
